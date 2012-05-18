47d18d0668a80789062dc072b276745d6540a176
Allen <abierbaum@gmail.com>
2012-05-18 07:15:57 -0500

    Update with correct filter behavior for html replacement.

--- build_assets.py
+++ build_assets.py
@@ -1,6 +1,8 @@
 import logging
 import os
+import re
 import sys
+
 pj = os.path.join
 
 # Setup the paths to the dependencies
@@ -17,12 +19,21 @@ gAssetUrl = '/static'
 
 class HtmlFilter(Filter):
    """
-   When complete, this filter will allow the user to filter html files into
-   static output html files that include references to the Javascript and CSS
-   files that should be loaded.  The goal is that when run in debug mode, this
-   would reference the source js files and the uncompressed CSS files (possibly compiled).
+   his filter allows the user to filter html files into static output html files
+   that include references to the Javascript and CSS files that should be loaded.
+   The goal is that when run in debug mode, this would reference the source js files
+   and the uncompressed CSS files (possibly compiled).
+
+   Looks for the following TAGS in the html file:
+     * {{CSS_LINKS}}: Replaced with <link> elements to each css file
+     * {{JS_LINKS}}: Replaced with <script> elements to each js file.
    """
    name = 'html'
+
+   def __init__(self, css = None, js = None, *a, **kw):
+      self.css = css or []
+      self.js  = js  or []
+      super(HtmlFilter, self).__init__(*a, **kw)
 
    def output(self, _in, out, **kwds):
       """ Called with concatenated inputs. """
@@ -30,12 +41,25 @@ class HtmlFilter(Filter):
 
    def input(self, _in, out, **kwds):
       """ Called to filter the inputs to this filter. """
-      print "WAS HERE"
-      out.write(_in.read())
-      # NOTE: Yet to write this code to replace with CSS and JS references
-      out.write('<!-- Was Filtered -->')
-      #for b in self.env:
-      #   out.write('<!-- %s -->' % b.urls())
+      content = _in.read()
+
+      # Build up content to replace
+      css_code = ''
+      js_code  = ''
+
+      for css_name in self.css:
+         for url in self.env[css_name].urls():
+            css_code += '<link rel="stylesheet" href="%s" type="text/css"/>\n' % url
+
+      for js_name in self.js:
+         for url in self.env[js_name].urls():
+            js_code += '<script type="text/javascript" src="%s"></script>\n' % url
+
+      content = re.sub("{{\s*?JS_LINKS\s*?}}", js_code, content)
+      content = re.sub("{{\s*?CSS_LINKS\s*?}}", css_code, content)
+
+      out.write(content)
+
 
 register_filter(HtmlFilter)
 
@@ -43,11 +67,13 @@ register_filter(HtmlFilter)
 def out(*args):
    return pj('build', *args)
 
+def html_filter(css, js):
+   return HtmlFilter(css=css, js=js)
 
 def setup_env(debug=True, cache=False):
    env = Environment(gAssetDir, gAssetUrl)
 
-   env.debug    = debug   
+   env.debug    = debug
    env.cache    = cache
    env.manifest = False
 
@@ -55,8 +81,9 @@ def setup_env(debug=True, cache=False):
    sass = Bundle('sass/test.scss', filters='compass', output=out('css', 'test.css'),
                     debug = False)
    css  = Bundle(sass, 'style.css', output=out('css', 'min.css'))
-   html = Bundle('test.html', filters='html', output=out('test.html'))
    js   = Bundle('src/test_app.js', filters='rjsmin', output=out('test_app_min.js'))
+   html = Bundle('test.html', output=out('test.html'),
+                 filters = html_filter(css=['css'], js=['js']))
 
    # Register bundles
    env.register('css',  css)
@@ -81,19 +108,19 @@ def build():
    env = setup_env()
    log = load_logger()
 
-   print "Urls [debug]: "
+   print "Urls [before build]: "
    _printUrls(env)
-   
+
    cmdenv = CommandLineEnvironment(env, log)
    cmdenv.build()
 
-   print "Urls [prod]: "
+   print "Urls [after build]: "
    _printUrls(env)
-      
+
    # Attempted another way to do this, but still can't build in debug mode
    #for b in env:
    #   b.build(force=True, env=env, disable_cache=(not cache))
-   
+
 
 if __name__ == '__main__':
    build()
